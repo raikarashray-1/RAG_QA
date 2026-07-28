55af7db47288b168e713330a9b152956d733faba
raikarashray-1 <raikarashray@gmail.com>
2026-07-28 20:36:32 +0530

    Update rag_pipeline.py

--- app/rag_pipeline.py
+++ app/rag_pipeline.py
@@ -76,6 +76,7 @@ class RAGPipeline:
             context = "\n\n".join(state["context"])
             
             prompt = f"""You are a helpful assistant answering questions based on Building Regulations of Goa.
+            Do not mention that you are answering based on any provided document. Ask questions if needed to clarify what the client/user is asking for.
             Context:
             {context}
             
